7c18309cea499c4c1fa6794e045a2afb53e24c6e
JasonBai007 <byygkcg@163.com>
2016-05-11 16:33:14 +0800

    添加计时器组件

--- app/components/hello/hello.jsx
+++ app/components/hello/hello.jsx
@@ -9,7 +9,7 @@ export default class Hello extends React.Component {
   }
   render() {
     return (
-      <h1>Hello World</h1>
+      <h1>Hello {this.props.name}</h1>
     );
   }
 }
--- app/main.jsx
+++ app/main.jsx
@@ -4,7 +4,9 @@ import './main.css';
 import Hello from './components/hello/hello.jsx';
 import Range from './components/range/range.jsx';
 import Exa from './components/example/example.jsx';
+import Timer from './components/timer/timer.jsx';
 
-ReactDOM.render( <Hello />, document.querySelector('#hello'));
+ReactDOM.render( <Hello name="Jason Bai"/>, document.querySelector('#hello'));
 ReactDOM.render( <Range />, document.querySelector('#range'));
 ReactDOM.render( <Exa />, document.querySelector('#example'));
+ReactDOM.render( <Timer />, document.querySelector('#timer'));
